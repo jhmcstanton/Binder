7c91d984372a0f236cf9cd92f85fa282eb0f689f
Jim Stanton <jim@jhstanton.com>
2016-02-24 08:25:58 -0600

    Updated some html handling

--- src/Binder/File.hs
+++ src/Binder/File.hs
@@ -47,20 +47,25 @@ collectBinder = do
 
 buildBinder :: Binder (Maybe Object) (T.Text, T.Text) -> Html
 buildBinder binder@(Binder base _ _ _) = 
-  let (contents, marks) = unzip (op binder) in mkToC (foldr appendToC mempty contents) <> fold marks where 
+  let (contents, marks) = unzip (op 0 binder) in mkToC (foldr (appendToC 0) mempty contents) <> fold marks where 
   mkNote :: T.Text -> T.Text -> Html
   mkNote name markdownText = mkSection $ (h1 ! Attr.class_ "note" ! Attr.id (lazyTextValue name) $ toHtml name) <> (markdown def markdownText)
-  mkToC contents = (h2 ! Attr.id (textValue "ToC") $ toHtml (T.pack "Table of Contents")) <> (ul ! Attr.id "toc-list"  $ contents)
+  mkToC contents = (h2 ! Attr.id (textValue "ToC") $ toHtml (T.pack "Table of Contents")) <> (ul ! Attr.class_ "toc-list"  $ contents)
   mkSection :: Html -> Html
   mkSection = div ! Attr.class_ "section" 
   mkJump :: T.Text -> T.Text
   mkJump name = "./binder.html#" <> name -- this will probably need to be updated
-  appendToC :: T.Text -> Html -> Html -- at some point this will need to have section #s   
-  appendToC name currentToC = (a ! Attr.href (lazyTextValue $ mkJump name ) $ li ! Attr.class_ "ToC_entry" $ toHtml name) <> currentToC
-  op :: Binder (Maybe Object) (T.Text, T.Text) -> [(T.Text, Html)]
-  op (Binder _ _ notes binders) = 
+  appendToC :: Int -> T.Text -> Html -> Html -- at some point this will need to have section #s   
+  appendToC depth name currentToC =  (a 
+     ! Attr.class_ (stringValue $ innerTocName depth)
+     ! Attr.href (lazyTextValue $ mkJump name ) $ li 
+     ! Attr.class_ "ToC_entry" $ toHtml name) <> currentToC
+  op :: Int -> Binder (Maybe Object) (T.Text, T.Text) -> [(T.Text, Html)]
+  op depth (Binder _ _ notes binders) = 
     fmap (\(name, mark) -> (name, mkNote name mark)) notes <> 
-      foldr (\binder acc -> acc ++ op binder) [] binders
+      foldr (\binder acc -> acc ++ op (depth + 1) binder) [] binders
 
 addHeader :: Html -> Html
-addHeader html = (head $ link ! Attr.href "style.css" ! Attr.rel "stylesheet" ! Attr.type_ "text/css") <> html
+addHeader html = header <> html where 
+  header = head $ headerContents
+  headerContents = link ! Attr.href "style.css" ! Attr.rel "stylesheet" ! Attr.type_ "text/css" <> title "Binder!"
